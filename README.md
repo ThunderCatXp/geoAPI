c8170cf746462bc0f52588567982dd3924c49a78
Thunder Cat <thundercatxp@yahoo.com>
2024-05-12 13:09:40 +0300

    update

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ npm run dev
 opened http://localhost:3000
 ```
 
-API output example:
+API output example(with formating):
 
 ```json
 {
--- prisma/db.ts
+++ prisma/db.ts
@@ -1,34 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 import CircularJSON from 'circular-json'
-/*
-async function main() {
-    
-    const result = await prisma.location.findFirst(
-        {
-            where: {
-                city: "Herat"
-            }
-        }
-    )
-    console.log(result)
-
-    
-    
-}
-*/
-
-
-
-/*
-main().catch(e=> {
-    console.error(e.message)
-})
-.finally(async () => {
-    await prisma.$disconnect()
-})
-*/
-
 
 export async function getCity(city: string) {
     const result = await prisma.location.findFirst(
